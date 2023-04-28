f7cc57cf2d207a026e8cb3d6316366a74a906722
P V Ajay Thota <pvajay.thota@gmail.com>
2023-04-28 22:50:52 +0530

    Update utils.py
    removing sensitive information

--- shared/utils.py
+++ shared/utils.py
@@ -3,7 +3,7 @@ from azure.storage.blob import BlobServiceClient,ContainerClient,ContentSettings
 
 def upload_pickle_to_blob(pkl):
 
-    BLOB_STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=automation9e98;AccountKey=lePx6UTRq6nZPFF4Cg3VHDT1m1R5RqAklPRcU6960idIwtsvGr6L5a+iUexLuEegrUmHUij002iT+AStbuIPxA==;EndpointSuffix=core.windows.net"
+    BLOB_STORAGE_CONNECTION_STRING = "---------------------------------------------------------------------------------------------"
     # BLOB_STORAGE_CONNECTION_STRING = "UseDevelopmentStorage=true;DevelopmentStorageProxyUri=http://azurite"
     try:
         bsc = BlobServiceClient.from_connection_string(BLOB_STORAGE_CONNECTION_STRING)
